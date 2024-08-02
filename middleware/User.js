2e10650fe27856830c5246167efb85b988e744b0
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-08-02 21:33:14 +0530

    Login and middleware for cart

---
+++ middleware/User.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import 'dotenv/config';
+
+export const authenticateToken = (req, res, next) => {
+  const token = req.cookies.Token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Access Denied" });
+  }
+
+  try {
+    const secretKey = process.env.SECRET_KEY;
+    const verified = jwt.verify(token, secretKey);
+    req.user = verified;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid Token" });
+  }
+};
+
+
