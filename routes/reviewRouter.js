f0fc5529a24b58944d7d3c1c49686e936d5f163e
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-12-05 19:53:54 +0530

    Delete Reviews

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   addReview,
+  DeleteReview,
   getReviewsByProduct,
   updateReview,
 } from "../controllers/reviewController.js";
@@ -14,5 +15,7 @@ router.get("/getreview/:id", getReviewsByProduct);
 //! New router for Update reviews
 
 router.put("/updateReview", updateReview);
+
+router.delete("/deleteReview", DeleteReview);
 
 export default router;
