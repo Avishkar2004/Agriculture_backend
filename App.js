303a66f8cbb930ff6ecc0f38a412e1e58cf0371d
Avishkar2004 <avishkarkakde2004@gmail.com>
2025-01-14 18:04:23 +0530

    Cache Via Redis

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ import { db } from "./config/db.js";
 import session from "express-session";
 import http from "http";
 import { Server } from "socket.io";
-
 import { authenticateToken } from "./middleware/User.js";
 import { getOrganicProducts } from "./models/organicproduct.js";
 import {
@@ -167,12 +166,12 @@ if (cluster.isPrimary) {
   app.get("/fungicides", cacheMiddleware, Fungicides);
 
   // For Review's
-  app.use("/api/reviews", reviewRouter);
+  app.use("/api/reviews", cacheMiddleware, reviewRouter);
 
   //! For order/ placedOrders info
   app.use("/api", authenticateToken, orderRoutes);
 
-  app.use("/api/delivery-address", deliveryAddressRoutes);
+  app.use("/api/delivery-address", cacheMiddleware, deliveryAddressRoutes);
   app.use("/api", deliveryAddressRoutes);
 
   app.get("/plantgrowthregulator/next/:id", getNextProductplantgrowthregulator);
